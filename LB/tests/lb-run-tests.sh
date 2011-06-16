5bc7fe3e3ecc42b75725c97f50d27d1f0527ceb6
Zdeněk Šustr <sustr4@cesnet.cz>
2011-06-16 13:18:58 +0000

    Fix hardcoded testbed name.

--- LB/tests/lb-run-tests.sh
+++ LB/tests/lb-run-tests.sh
@@ -213,7 +213,7 @@ echo sh ./lb-test-changeacl.sh \$OUTPUT_OPT >> arrange_lb_test_user.sh
 echo sh ./lb-test-statistics.sh \$OUTPUT_OPT >> arrange_lb_test_user.sh
 echo sh ./lb-test-threaded.sh \$OUTPUT_OPT >> arrange_lb_test_user.sh
 echo sh ./lb-test-harvester.sh \$OUTPUT_OPT >> arrange_lb_test_user.sh
-echo perl ./lb-test-purge.pl --i-want-to-purge delwin.fi.muni.cz:9000 \$OUTPUT_OPT >> arrange_lb_test_user.sh
+echo perl ./lb-test-purge.pl --i-want-to-purge $remotehost:9000 \$OUTPUT_OPT >> arrange_lb_test_user.sh
 echo 'echo "</literal>"' >> arrange_lb_test_user.sh
 echo 'echo "<verbatim>"' >> arrange_lb_test_user.sh
 echo echo ================== >> arrange_lb_test_user.sh
