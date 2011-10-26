1afe50e0acea0551645392a2bdc40fa2f0987ef5
Zdeněk Šustr <sustr4@cesnet.cz>
2011-10-26 12:12:41 +0000

    Proper escaping

--- LB/tests/lb-common-testbeds.sh
+++ LB/tests/lb-common-testbeds.sh
@@ -66,7 +66,7 @@ else
 	rm -rf /tmp/test-certs/grid-security
 	cvs -d :pserver:anonymous@glite.cvs.cern.ch:/cvs/jra1mw co org.glite.testsuites.ctb/LB
 	FAKE_CAS=\`./org.glite.testsuites.ctb/LB/tests/lb-generate-fake-proxy.sh | grep -E "^X509_CERT_DIR" | sed 's/X509_CERT_DIR=//'\`
-	cp -rv $FAKE_CAS/* /etc/grid-security/certificates/
+	cp -rv \$FAKE_CAS/* /etc/grid-security/certificates/
 fi
 
 CVSPATH=\`which cvs\`
