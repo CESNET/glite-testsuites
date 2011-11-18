0abf80c29e7713420c8babb6c4b378eacd0fd1b2
František Dvořák <valtri@civ.zcu.cz>
2011-11-18 00:44:45 +0000

    Create example installation script only as default (installation from production) when don't exist.

--- LB/tests/lb-autonomous-test.sh
+++ LB/tests/lb-autonomous-test.sh
@@ -56,9 +56,9 @@ source lb-common-testbeds.sh
 STARTTIME=`date +%s`
 
 printf "Getting the 'install' script... "
-#XXX Provisional. The test won't be generated here in the future. Just downloaded or otherwise obtained
-SCENARIO="Clean installation"
-cat << EndInstallScript > LBinstall.sh 
+# Example script, for real tests it should be downloaded or otherwise obtained
+SCENARIO=${SCENARIO:-"Clean installation"}
+test -s LBinstall.sh || cat << EndInstallScript > LBinstall.sh 
 rpm -Uvhi http://download.fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
 yum install -y yum-priorities yum-protectbase
 rpm -i http://emisoft.web.cern.ch/emisoft/dist/EMI/1/sl5/x86_64/base/emi-release-1.0.0-1.sl5.noarch.rpm
--- gridsite/tests/gridsite-autonomous-test.sh
+++ gridsite/tests/gridsite-autonomous-test.sh
@@ -68,9 +68,9 @@ source lb-common-testbeds.sh
 
 
 printf "Getting the 'install' script... "
-#XXX Provisional. The test won't be generated here in the future. Just downloaded or otherwise obtained
-SCENARIO="Clean installation"
-cat << EndInstallScript > GridSiteInstall.sh
+# Example script, for real tests it should be downloaded or otherwise obtained
+SCENARIO=${SCENARIO:-"Clean installation"}
+test -s GridSiteInstall.sh || cat << EndInstallScript > GridSiteInstall.sh
 rpm -Uvhi http://download.fedora.redhat.com/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
 yum install -y yum-priorities yum-protectbase
 rpm -i http://emisoft.web.cern.ch/emisoft/dist/EMI/1/sl5/x86_64/base/emi-release-1.0.0-1.sl5.noarch.rpm
