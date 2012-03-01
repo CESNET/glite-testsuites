30be9f5e2dd7c00a6fac2174e800ee31698e2a45
Zdeněk Šustr <sustr4@cesnet.cz>
2012-03-01 15:35:51 +0000

    Actually assign to group and assign role

--- PX/tests/px-voms-install.sh
+++ PX/tests/px-voms-install.sh
@@ -20,7 +20,8 @@ function add_voms_user_w_attrs() {
 	voms-admin --nousercert --vo vo.org create-user "$1" "$2" "$3" "$4"
 	voms-admin --nousercert --vo vo.org set-user-attribute "$1" "$2" attribute1 $RANDOM
 	voms-admin --nousercert --vo vo.org set-user-attribute "$1" "$2" attribute2 $RANDOM
-	voms-admin --nousercert --vo vo.org add-member Testers "$1" "$2"
+	voms-admin --nousercert --vo vo.org add-member "/vo.org/Testers" "$1" "$2"
+	voms-admin --nousercert --vo vo.org assign-role "/vo.org/Testers" Tester "$1" "$2"
 }
 
 USERNAME="root"
